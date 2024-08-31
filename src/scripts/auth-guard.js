c383f42e3d7239d985a3d4417bab3c1cb168acbf
Diogo Paz <diogoachiles@gmail.com>
2024-08-31 00:49:48 -0300

    restringindo as páginas de controle apenas para o admin

--- src/scripts/auth-guard.js
+++ src/scripts/auth-guard.js
@@ -1,5 +1,20 @@
 firebase.auth().onAuthStateChanged((user) => {
   if (!user) {
     window.location.href = "../../pages/tela_login";
+  } else if (user.email !== "admin@admin.com") {
+    const restrictedPages = [
+      "admin",
+      "tela_cadastro_materiais",
+      "tela_gerenciar_materiais",
+    ];
+    const currentPath = window.location.pathname.split("/");
+    currentPath.pop();
+    currentPath.shift();
+
+    restrictedPages.forEach((page) => {
+      if (currentPath.includes(page)) {
+        window.location.href = "../../pages/tela_inicial";
+      }
+    });
   }
 });
