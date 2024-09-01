d53236404c422b5b9540248a7757d8dc0c8cb371
Diogo Paz <diogoachiles@gmail.com>
2024-08-31 21:32:50 -0300

    implementando a pesquisa de materiais salvos no banco de dados

--- src/scripts/material-search.js
+++ src/scripts/material-search.js
@@ -1,36 +1,55 @@
-document.getElementById('searchMaterials-form').addEventListener('submit', function(event) {
+document
+  .getElementById("searchMaterials-form")
+  .addEventListener("submit", async function (event) {
     event.preventDefault();
 
     function removerAcentos(str) {
-        return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+      return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
     }
 
-    const searchTerm = removerAcentos(document.getElementById('search').value.toLowerCase());
-
-    //codigo teste de materiais
-    const materials = [
-        { id: '1', title: 'Livro A', description: 'Descrição do Livro A', subject: 'Matemática', author: 'Autor A' },
-        { id: '2', title: 'Livro B', description: 'Descrição do Livro B', subject: 'Física', author: 'Autor B' },
-        { id: '1', title: 'Livro c', description: 'Descrição do Livro c', subject: 'Matemática', author: 'Autor c' },
-    ];
-
-
-    const filteredMaterials = materials.filter(material => 
-    removerAcentos(material.title.toLowerCase()).includes(searchTerm) ||
-    removerAcentos(material.description.toLowerCase()).includes(searchTerm) ||
-    removerAcentos(material.subject.toLowerCase()).includes(searchTerm) ||
-    removerAcentos(material.author.toLowerCase()).includes(searchTerm)
+    const searchTerm = removerAcentos(
+      document.getElementById("search").value.toLowerCase()
     );
 
+    //codigo teste de materiais
+    let materials = [];
+    await firebase
+      .firestore()
+      .collection("study-materials")
+      .get()
+      .then((snapshot) => {
+        snapshot.docs.map((doc) => {
+          const data = doc.data();
+          const material = {
+            id: doc.id,
+            title: data.title,
+            description: data.description,
+            subject: data.subject.id,
+            author: data.author,
+          };
+          materials.push(material);
+          return material;
+        });
+      });
 
-    const resultsDiv = document.getElementById('results');
-    resultsDiv.innerHTML = ''; 
+    const filteredMaterials = materials.filter(
+      (material) =>
+        removerAcentos(material.title.toLowerCase()).includes(searchTerm) ||
+        removerAcentos(material.description.toLowerCase()).includes(
+          searchTerm
+        ) ||
+        removerAcentos(material.subject.toLowerCase()).includes(searchTerm) ||
+        removerAcentos(material.author.toLowerCase()).includes(searchTerm)
+    );
+
+    const resultsDiv = document.getElementById("results");
+    resultsDiv.innerHTML = "";
 
     if (filteredMaterials.length > 0) {
-        filteredMaterials.forEach(material => {
-            const materialDiv = document.createElement('div');
-            materialDiv.className = 'material-item';
-            materialDiv.innerHTML = `
+      filteredMaterials.forEach((material) => {
+        const materialDiv = document.createElement("div");
+        materialDiv.className = "material-item";
+        materialDiv.innerHTML = `
                 <h2>${material.title}</h2>
                 <p>ID: ${material.id}</p>
                 <p>Descrição: ${material.description}</p>
@@ -38,14 +57,14 @@ document.getElementById('searchMaterials-form').addEventListener('submit', funct
                 <p>Autor: ${material.author}</p>
                 <button onclick="adicionarBiblioteca('${material.id}')">adicionar a biblioteca</button>
             `;
-            resultsDiv.appendChild(materialDiv);
-        });
+        resultsDiv.appendChild(materialDiv);
+      });
     } else {
-        resultsDiv.innerHTML = '<p>Nenhum material encontrado.</p>';
+      resultsDiv.innerHTML = "<p>Nenhum material encontrado.</p>";
     }
-});
+  });
 
 function adicionarBiblioteca(id) {
-    alert(`Você adicionou a biblioteca o material com o ID ${id}`);
-    //codigo ainda a implementar para adicionar um livro a biblioteca do usuario
-}                                                                         
+  alert(`Você adicionou a biblioteca o material com o ID ${id}`);
+  //codigo ainda a implementar para adicionar um livro a biblioteca do usuario
+}
